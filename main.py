698b4ceb1508f463602f850d199659f5c309bfb5
irvan.april15 <irvan.april15@gmail.com>
2024-09-14 18:52:04 +0700

    add config jwt and model data

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
+from typing import Optional
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
-app = FastAPI()
+SECRET_KEY = "b1d4733aa6daf70e186e2131b74452ab13e847821c5cedc637336db5ee01e49c"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+
+dumy_db = {
+    'jon' : {
+        'username' : 'jon',
+        'full_name' : 'John Canada',
+        'email' : 'joh@mail.com',
+        'hashed_password' : '',
+        'disable' : False
+    }
+}
 
 class Data(BaseModel):
     name: str
 
+class Token(BaseModel):
+    access_token : str
+    toke_type : str
+
+class TokenData(BaseModel):
+    username : Optional[str] = Field(default=None)
+
+class User(BaseModel):
+    username : str 
+    email : Optional[str] = Field(default=None)
+    full_name : Optional[str] = Field(default=None)
+    disable : Optional[str] = Field(default=None)
+
+class UserInDB(User):
+    hashed_password : str
+
+pwd_context =  CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+app = FastAPI()
 
 @app.post('/create/')
 async def create(data: Data):
