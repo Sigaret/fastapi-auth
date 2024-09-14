b68b195fda8a09836a49c831ddfec69ce42a7a88
irvan.april15 <irvan.april15@gmail.com>
2024-09-14 19:34:22 +0700

    add method get current active user

--- main.py
+++ main.py
@@ -77,6 +77,31 @@ def create_access_token(data: dict, expires_delta : Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+async def get_current_user(token: str = Depends(oauth_2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could Not validate Credential', headers={'WWW-Authenticate' : 'Bearer Token'})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username : str = payload.get('sub')
+        if username is None:
+            raise credential_exception
+        
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        return credential_exception
+    
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+    
+    return user
+
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disable:
+        raise HTTPException(status_code=400, detail='Inactive User')
+    
+    return current_user
+
 @app.post('/create/')
 async def create(data: Data):
     return{"data" : data}
